597228b6ab68b9b85689793ae664a1c542a54bf1
Dominic DiGiacomo <dominic.m.digiacomo@gmail.com>
2023-02-07 00:46:10 -0500

    diag v.6

--- lua/noice/lsp/diagnostic.lua
+++ lua/noice/lsp/diagnostic.lua
@@ -4,6 +4,8 @@ local Format = require("noice.lsp.format")
 local Util = require("noice.util")
 local Docs = require("noice.lsp.docs")
 
+local diag = vim.diagnostic
+
 local api, if_nil = vim.api, vim.F.if_nil
 
 local global_diagnostic_options = {
@@ -12,156 +14,10 @@ local global_diagnostic_options = {
   virtual_text = true,
   float = true,
   update_in_insert = false,
-  severity_sort = false,
+  severity_sort = true,
 }
 
-local diagnostic_cache
-do
-  local group = api.nvim_create_augroup('DiagnosticBufWipeout', {})
-  diagnostic_cache = setmetatable({}, {
-    __index = function(t, bufnr)
-      assert(bufnr > 0, 'Invalid buffer number')
-      api.nvim_create_autocmd('BufWipeout', {
-        group = group,
-        buffer = bufnr,
-        callback = function()
-          rawset(t, bufnr, nil)
-        end,
-      })
-      t[bufnr] = {}
-      return t[bufnr]
-    end,
-  })
-end
-
-local function get_bufnr(bufnr)
-  if not bufnr or bufnr == 0 then
-    return api.nvim_get_current_buf()
-  end
-  return bufnr
-end
-
-local function enabled_value(option, namespace)
-  local ns = namespace and M.get_namespace(namespace) or {}
-  if ns.opts and type(ns.opts[option]) == 'table' then
-    return ns.opts[option]
-  end
-
-  if type(global_diagnostic_options[option]) == 'table' then
-    return global_diagnostic_options[option]
-  end
-
-  return {}
-end
-
-local function resolve_optional_value(option, value, namespace, bufnr)
-  if not value then
-    return false
-  elseif value == true then
-    return enabled_value(option, namespace)
-  elseif type(value) == 'function' then
-    local val = value(namespace, bufnr)
-    if val == true then
-      return enabled_value(option, namespace)
-    else
-      return val
-    end
-  elseif type(value) == 'table' then
-    return value
-  else
-    error('Unexpected option type: ' .. vim.inspect(value))
-  end
-end
-
-local function get_resolved_options(opts, namespace, bufnr)
-  local ns = namespace and M.get_namespace(namespace) or {}
-  -- Do not use tbl_deep_extend so that an empty table can be used to reset to default values
-  local resolved = vim.tbl_extend('keep', opts or {}, ns.opts or {}, global_diagnostic_options)
-  for k in pairs(global_diagnostic_options) do
-    if resolved[k] ~= nil then
-      resolved[k] = resolve_optional_value(k, resolved[k], namespace, bufnr)
-    end
-  end
-  return resolved
-end
-
-
-
-local function get_diagnostics(bufnr, opts, clamp)
-  opts = opts or {}
-
-  local namespace = opts.namespace
-  local diagnostics = {}
-
-  -- Memoized results of buf_line_count per bufnr
-  local buf_line_count = setmetatable({}, {
-    __index = function(t, k)
-      t[k] = api.nvim_buf_line_count(k)
-      return rawget(t, k)
-    end,
-  })
-
-  ---@private
-  local function add(b, d)
-    if not opts.lnum or d.lnum == opts.lnum then
-      if clamp and api.nvim_buf_is_loaded(b) then
-        local line_count = buf_line_count[b] - 1
-        if
-          d.lnum > line_count
-          or d.end_lnum > line_count
-          or d.lnum < 0
-          or d.end_lnum < 0
-          or d.col < 0
-          or d.end_col < 0
-        then
-          d = vim.deepcopy(d)
-          d.lnum = math.max(math.min(d.lnum, line_count), 0)
-          d.end_lnum = math.max(math.min(d.end_lnum, line_count), 0)
-          d.col = math.max(d.col, 0)
-          d.end_col = math.max(d.end_col, 0)
-        end
-      end
-      table.insert(diagnostics, d)
-    end
-  end
-  
-  ---@private
-  local function add_all_diags(buf, diags)
-    for _, diagnostic in pairs(diags) do
-      add(buf, diagnostic)
-    end
-  end
-
-  if namespace == nil and bufnr == nil then
-    for b, t in pairs(diagnostic_cache) do
-      for _, v in pairs(t) do
-        add_all_diags(b, v)
-      end
-    end
-  elseif namespace == nil then
-    bufnr = get_bufnr(bufnr)
-    for iter_namespace in pairs(diagnostic_cache[bufnr]) do
-      add_all_diags(bufnr, diagnostic_cache[bufnr][iter_namespace])
-    end
-  elseif bufnr == nil then
-    for b, t in pairs(diagnostic_cache) do
-      add_all_diags(b, t[namespace] or {})
-    end
-  else
-    bufnr = get_bufnr(bufnr)
-    add_all_diags(bufnr, diagnostic_cache[bufnr][namespace] or {})
-  end
-
-  if opts.severity then
-    diagnostics = filter_by_severity(opts.severity, diagnostics)
-  end
-
-  return diagnostics
-end
-
-
 local M = {}
-
 
 function M.setup()
   vim.lsp.handlers["textDocument/publishDiagnostics"] = M.open_float
@@ -212,11 +68,12 @@ function M.open_float(opts, ...)
   end
 
   print(bufnr, scope, lnum)
-  local diagnostics = get_diagnostics(bufnr, opts, true)
+  local diagnostics = diag.get(bufnr, {
+    lnum = lnum,
+  })
 
   if scope == 'line' then
     diagnostics = vim.tbl_filter(function(d)
-      print(bufnr, scope, lnum, d.lnum)
       return d.lnum == lnum
     end, diagnostics)
   elseif scope == 'cursor' then
