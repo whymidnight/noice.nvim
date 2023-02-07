117849a502119144c15d2a71f9225a5d71e47e56
Dominic DiGiacomo <dominic.m.digiacomo@gmail.com>
2023-02-07 01:55:18 -0500

    diag v1.1.8

--- lua/noice/lsp/hover.lua
+++ lua/noice/lsp/hover.lua
@@ -3,7 +3,16 @@ local require = require("noice.util.lazy")
 local Format = require("noice.lsp.format")
 local Util = require("noice.util")
 local Docs = require("noice.lsp.docs")
-local Diag = require("noice.lsp.diagnostic")
+local Diag = noice.lsp.diag
+
+local function tableMerge(table1, table2, result)
+	for _, v in ipairs(table1) do
+		table.insert(result, v)
+	end
+	for _, v in ipairs(table2) do
+		table.insert(result, v)
+	end
+end
 
 local M = {}
 
@@ -13,29 +22,22 @@ end
 
 function M.on_hover(_, result)
   if not (result and result.contents) then
-    print("empty hover result")
+    print("no result and contents")
     return
   end
 
-  local hover_contents
+  local hover_contents = {}
 
   local message = Docs.get("hover")
 
   local diagnostic = Diag.get_diagnostic(nil)
   if not vim.tbl_isempty(diagnostic) then
-    hover_contents = diagnostic or {}
-    for _, v in ipairs(result.contents) do
-      table.insert(hover_contents, v)
-    end
-  else
-    hover_contents = {}
-    for _, v in ipairs(result.contents) do
-      table.insert(hover_contents, v)
-    end
+    print("diag")
+    tableMerge(Format.format_markdown(diagnostic), {}, hover_contents)
   end
 
-  print(hover_contents)
-
+  print("result contents", vim.tbl_isempty(result.contents))
+  tableMerge({}, result.contents, hover_contents)
   if not message:focus() then
     Format.format(message, hover_contents)
     if message:is_empty() then
